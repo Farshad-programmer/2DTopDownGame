5ca993a793ac74c09eee91e22b13450e536673e5
Farshad Shirkhani <63773984+Farshad-programmer@users.noreply.github.com>
2023-09-26 16:34:06 +0300

    sword animation and attack input implemented

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private Animator anim;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    private void Awake()
+    {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+        playerControls = new PlayerControls();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Start()
+    {
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0f, -180f, angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
+    }
+}
